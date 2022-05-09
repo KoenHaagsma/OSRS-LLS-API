e887a726499cc0cea9edcdb9a6f417643348c555
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-05-09 13:19:02 +0200

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', routes);
